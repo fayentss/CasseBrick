af9cb029eea44212bb3dbee635bb1c7b2eb088cd
Fusix10 <115464978+Fusix10@users.noreply.github.com>
2023-11-07 19:07:23 +0100

    Pre Collider start
    gl ma couille

--- CasseBrick/Ball.cpp
+++ CasseBrick/Ball.cpp
@@ -1,14 +1,45 @@
 #include "Ball.h"
 #include "GameObj.h"
+#include <iostream>;
 
 Ball::Ball(int iDiametre, int iPosX, int iPosY, sf::Color cColor) : GameObj(iDiametre, iPosX, iPosY, cColor)
 {
 	// pos initial
 	//
 }
-void Ball::Collider() 
+void Ball::Collider(sf::RenderWindow oWindow, int iPosX, int iPosY)
 {
-	if () 
+	if ( iPosX > oWindow.getSize().x )
+	{
+		std::cout << "colide a droit connard";
+	}
+	else if (iPosX < 0)
+	{
+		std::cout << "colide a gauche connard";
+	}
+
+	if (iPosY > oWindow.getSize().y)
+	{
+		std::cout << "colide en haut connard";
+	}
+	else if (iPosY < 0)
+	{
+		std::cout << "colide en bas connard";
+	}
+
+	if (iPosX < 0 && iPosY < 0) 
+	{
+		std::cout << "colide coin en bas a gauche connard";
+	}
+	else if (iPosX < oWindow.getSize().x && iPosY < oWindow.getSize().y) 
+	{
+		std::cout << "colide coin en haut a droit connard";
+	}
+	else if (iPosX < 0 && iPosY < oWindow.getSize().y)
+	{
+		std::cout << "colide coin en haut a gauche connard";
+	}
+	else if (iPosX < 0 && iPosY < 0) 
 	{
 
 	}
--- CasseBrick/Ball.h
+++ CasseBrick/Ball.h
@@ -8,6 +8,6 @@ private:
 
 public:
 	Ball(int iDiametre, int iPosX, int iPosY, sf::Color cColor);
-	void Collider();
+	void Collider(sf::RenderWindow oWindow, int iPosX, int iPosY);
 };
 
